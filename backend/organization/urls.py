643beb6116eaa8c0cc68fb3c993aab2ef4cc3329
Jethr0 <jethro0056@gmail.com>
2023-06-22 15:58:42 +0530

    docker integration

--- backend/organization/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("/getAll", views.getAllOrganizations),
-    path("/create", views.createOrganization),
-    path("/create-branch", views.createBranch),
-]
